0848decfeb138489571691f76ca5b76c45145d2e
Amit <amit.maurya@adjecti.com>
2023-04-24 15:26:55 +0530

    inital commit

---
+++ modules/jet-process-frontend/src/main/java/io/jetprocess/constants/JetProcessPortletKeys.java
@@ -0,0 +1,11 @@
+package io.jetprocess.constants;
+
+/**
+ * @author Admin
+ */
+public class JetProcessPortletKeys {
+
+	public static final String JETPROCESS =
+		"io_jetprocess_JetProcessPortlet";
+
+}
---
+++ modules/jet-process-frontend/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="jetprocess.caption"/></b>
+</p>
